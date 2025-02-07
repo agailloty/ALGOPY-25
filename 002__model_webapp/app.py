ee88eb92251af239d0e7ab9e2ea058bb82f6ce1b
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2025-02-07 09:32:27 +0100

    quelques donnée sdu formulaire

--- 002__model_webapp/app.py
+++ 002__model_webapp/app.py
@@ -1,3 +1,5 @@
 import streamlit as st
-
-st.markdown("# Streamlit opérationnel !")
+age = st.number_input("Âge", min_value=15, max_value=100)
+gender = st.selectbox("Sexe", ["Homme", "Femme"])
+annual_revenue = st.number_input("Revenu annuel ($)", min_value=0)
+marital_status = st.selectbox("Situation matrimoniale", ["Marié", "Célibataire", "Divorcé"])
