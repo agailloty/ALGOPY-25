78b1bb1dd0fad7ec5662bc700feb373d6af5641e
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2025-02-07 11:22:22 +0100

    conversion

--- 002__model_webapp/app.py
+++ 002__model_webapp/app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from formulaire import get_forms
+from model_helper import get_linear_model
+import numpy as np
+
+from dataclasses import asdict
+
+linear_model = get_linear_model()
 
 user_input = get_forms()
 
@@ -8,3 +14,8 @@ st.text(user_input)
 
 st.markdown("### Les données converties pour ML")
 st.text(user_input.convert_to_mlinput())
+
+
+mlinput = np.array([*asdict(user_input.convert_to_mlinput())]).reshape(1, -1)
+
+st.markdown(f"Le modèle prédit : {linear_model.predict(mlinput)} ")
--- 002__model_webapp/model_helper.py
+++ 002__model_webapp/model_helper.py
@@ -2,13 +2,15 @@ from pathlib import Path
 import os
 import pickle
 
+from sklearn.linear_model import LinearRegression
+
 rootPath = Path(__file__).parent
 
 modelPaths = rootPath.parent / "002__model_training" / "models"
 
 # print(os.listdir(modelPaths))
 
-def get_linear_model():
+def get_linear_model() -> LinearRegression:
     with open(modelPaths / 'LinearReg_Model.pkl', "rb") as f:
         model = pickle.load(f)
         return model
