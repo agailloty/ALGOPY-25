bf05163eac977a0272ef44b7ee28eecc775fa08d
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2025-02-27 20:02:17 +0100

    comparaison plusieurs modèles

--- 002__model_webapp/app.py
+++ 002__model_webapp/app.py
@@ -1,21 +1,50 @@
 import streamlit as st
 from formulaire import get_forms
-from model_helper import get_linear_model
+import model_helper as models
 import numpy as np
 
 from dataclasses import asdict
 
-linear_model = get_linear_model()
+LINEAR_REGRESSION = "Régression linéaire"
+GRADIENT_BOOSTING = "Gradient Boosting"
+KNN = "KNN"
+
+selected_model = st.selectbox("Choisir modèle prédiction",
+                              ["--", LINEAR_REGRESSION, GRADIENT_BOOSTING, KNN])
+
+if selected_model == LINEAR_REGRESSION:
+    model = models.get_linear_model()
+elif selected_model == GRADIENT_BOOSTING:
+    model = models.get_boosting_model()
+else :
+    model = models.get_knn_model()
+
 
 user_input = get_forms()
 
-st.markdown("### Les données que vous avez saisies")
-st.text(user_input)
-
-st.markdown("### Les données converties pour ML")
-st.text(user_input.convert_to_mlinput())
-
-
 mlinput = np.array([*asdict(user_input.convert_to_mlinput()).values()]).reshape(1, -1)
 
-st.markdown(f"Le modèle prédit : {linear_model.predict(mlinput)} ")
+prediction = ""
+
+def predict_model():
+    if selected_model != "--":
+        value_prediction = model.predict(mlinput)
+        global prediction
+        prediction = f"Le modèle {selected_model} prédit : {value_prediction}"
+        print(prediction)
+
+if selected_model != "--":
+    value_prediction = model.predict(mlinput)
+    prediction = f"Le modèle {selected_model} prédit : {value_prediction}"
+    st.markdown(f"### {prediction}")
+
+if st.checkbox("Comparaison"):
+    st.markdown("### Voici une comparaison de ce que prédit chaque modèle")
+
+    for model, prediction in zip([LINEAR_REGRESSION, GRADIENT_BOOSTING, KNN],
+                                 [models.get_linear_model().predict(mlinput),
+                                 models.get_boosting_model().predict(mlinput),
+                                 models.get_knn_model().predict(mlinput)]) :
+        
+        st.markdown(f"#### {model} : {prediction}")
+        
