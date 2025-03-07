8d9d3e7574658e22ced73f9e2e9c39485e3375da
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2025-03-07 09:35:47 +0100

    requetes xpath ok

---
+++ 003__webscraping/models.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Player:
+    name : str
+    age : int
+    overall_rating : int
+    height : str
+    weight : str
+    value_eur : str
+    weekly_wage : str
--- 003__webscraping/sofifa_extractor.py
+++ 003__webscraping/sofifa_extractor.py
@@ -1,22 +1,41 @@
 from pathlib import Path
 import os
 from typing import List
+from lxml.html import fromstring
+from models import Player
 
 root = Path(__file__).parent
 sofifa_files = root / "sofifa-html"
 
-def extract_data(filename : str) -> List[str]:
-    """Cette fonction prend en paramètre le chemin vers un fichier html, extraie son contenu et retourne une list de chaine de caractères"""
+def extract_players_data(filename : str) -> List[Player]:
+    """Cette fonction prend en paramètre le chemin vers un fichier html, extraie son contenu et retourne une list de joueurs"""
     with open(filename, "r", encoding="utf-8") as f:
-        content = "".join(f.readlines())
-        print(content)
+        content = fromstring("".join(f.readlines()))
+        player_names = content.xpath("//a[starts-with(@href, '/player/') and @aria-expanded='false']")
+        player_names = [player.text for player in player_names]
+        ages = content.xpath("//td[@data-col='ae']")
+        ages = [age.text for age in ages]
+        overall_ratings = [rating.text for rating in content.xpath("//td[@data-col='oa']/em")]
+        heights = [height.text for height in content.xpath("//td[@data-col='hi']")]
+        weights = [weight.text for weight in content.xpath("//td[@data-col='wi']")]
+        values_eur = [value.text for value in content.xpath("//td[@data-col='vl']")]
+        wages_eur = [value.text for value in content.xpath("//td[@data-col='wg']")]
+
+        players = []
+
+        for name, age, overall_rating, height, weight, value, wage in \
+              zip(player_names, ages, overall_ratings, heights, weights, values_eur, wages_eur):
+            player = Player(name, age, overall_rating, height, weight, value, wage)
+            players.append(player)
+    return players
+
 
 
 if os.path.exists(sofifa_files):
     files = os.listdir(sofifa_files)
     files = [sofifa_files / file for file in files]
 
-    extract_data(files[0])
+    extract_players_data(files[0])
 
 
 
